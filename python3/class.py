e538f9fde71fca4a83371ce3550742d4e3492a27
caonann <caon_1993@163.com>
2020-06-28 00:49:06 +0800

    update

---
+++ python3/class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+class TestName:
+    def __init__(self):
+        print(' init ')
+    def show_func(self):
+        print('this is python3 programe')
+
+if __name__ == "__main__":
+    a = TestName()
+    print('done')
