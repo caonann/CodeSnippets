1afad81eb12d63563dde1494922c69a854cf184e
caonan <caonann@qq.com>
2021-12-25 17:20:39 +0800

    update

---
+++ python3/README.md
@@ -0,0 +1,8 @@
+# 构建镜像
+```
+docker build -t pythonuint .
+```
+# 启动容器
+```
+sh run_docker.sh
+```
--- python3/snippets/dict_test.py
+++ python3/snippets/dict_test.py
@@ -26,3 +26,24 @@ print(dict_merge)
 ret = dict_merge.update({"update":"test"})
 print(dict_merge)
 print(ret)
+
+
+judgestr='ROOT'
+if judgestr in ('root','boot'):
+    print("hello")
+else:
+    print("not found")
+
+
+tmpmap={}
+tmpmap.setdefault("def", {}).setdefault("abc",[]).append("abc")
+tmpmap.setdefault("def", {}).setdefault("abc",[]).append("abc")
+tmpmap.setdefault("def", {}).setdefault("abc",[]).append("abc")
+tmpmap.setdefault("def", {}).setdefault("abc",[]).append("abc")
+tmpmap.setdefault("def", {}).setdefault("abc",[]).append("abc")
+tmpmap.setdefault("def", {}).setdefault("abc",[]).append("abc")
+print(tmpmap)
+print(tmpmap["def"]["abc"][2:])
+
+if " ":
+    print("yes")
