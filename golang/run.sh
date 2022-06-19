57ffedc3a3c8ec356c24022f8a7e818b2037bc31
caonan <caonann@qq.com>
2022-06-19 21:53:21 +0800

    update

--- golang/run.sh
+++ golang/run.sh
@@ -3,7 +3,7 @@ set -x
 name=golanguint
 containerid=`docker container ls -a -f "name=${name}" -q`
 if [ -z ${containerid} ];then
-    docker run -it --name ${name} --rm --cap-add=SYS_PTRACE --workdir /app --mount type=bind,source=$(pwd),target=/app ${name}
+    docker run -itd --name ${name} --cap-add=SYS_PTRACE --workdir /app --mount type=bind,source=$(pwd),target=/app ${name}
 else
     docker exec -it ${containerid} /bin/bash
 fi
