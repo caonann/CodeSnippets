2b4d5e4c14686eb3575b4787fe84ec2bf8ef5e89
hulkcao <hulkcao@tencent.com>
2022-11-20 17:44:01 +0800

    smart ptr

---
+++ cpp/smart_ptr/sharedptr_demo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <utility>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+class A {
+ public:
+  void show(){
+    for(int i=0;i<10;i++){
+      cout<<"show "<<i<<endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+  }
+  int a_ = 1;
+};
+
+std::vector<std::shared_ptr<A>> vas;
+
+void func_ptr(){
+  auto ptr = std::make_shared<A>();
+  cout << "use count" << ptr.use_count() << endl;
+
+  // vas.push_back(ptr);
+  vas.emplace_back(ptr);
+  auto t= std::thread([ptr]{
+    //ptr->show();
+  });
+  //t.join();
+  cout << "use count" << ptr.use_count() << endl;
+}
+
+int main() {
+  func_ptr();
+  cout<<"main func usecout"<<vas[0].use_count()<<endl;
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+  return 0;
+}
