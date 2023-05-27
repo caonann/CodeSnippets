684968f3f11489acb08a8a2252a88d65cd753300
caonan <caonann@qq.com>
2023-05-27 11:04:25 +0800

    update

--- python3/snippets/openai_demo/openai_proxy.py
+++ python3/snippets/openai_demo/openai_proxy.py
@@ -3,10 +3,10 @@ import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-domain = os.getenv("DOMAIN")
+#domain = os.getenv("DOMAIN")
 
 
-openai.api_base = f"http://{domain}/v1"
+#openai.api_base = f"http://{domain}/v1"
 # openai.api_type = "azure"
 # openai.api_version = "2023-03-15-preview"
 
@@ -14,7 +14,7 @@ completion = openai.ChatCompletion.create(
     # engine="gpt-35-turbo-0301",
     model="gpt-3.5-turbo",
     messages=[{"role": "user", "content": "Hello!"}],
-    stream=False,
+    stream=True,
 )
 
 # print(completion.choices[0].message)
